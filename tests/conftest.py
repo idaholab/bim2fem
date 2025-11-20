aa8d5ec78586b43cf1dc1f52cccbc273aec68774
Nicholas Crowder <Nicholas.Crowder@inl.gov>
2025-11-20 18:51:10 -0500

    remove unnecessary values from conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,13 +4,6 @@ import sys
 from pathlib import Path
 
 sys.path.insert(0, str(Path(__file__).parent.parent / "src"))
-import pytest
-import ifcopenshell
-import ifcopenshell.api.root
-import ifcopenshell.api.aggregate
-import ifcopenshell.api.spatial
-import ifcplus.api.project
-import ifcplus.api.placement
 from pathlib import Path
 
 TEST_DIR = Path(__file__).parent
@@ -35,77 +28,3 @@ OUTPUT_DIR_FOR_NUCLEAR.mkdir(exist_ok=True)
 
 OUTPUT_DIR_FOR_BUILT_ELEMENT = OUTPUT_DIR / "built_element"
 OUTPUT_DIR_FOR_BUILT_ELEMENT.mkdir(exist_ok=True)
-
-
-@pytest.fixture
-def empty_ifc_file() -> ifcopenshell.file:
-    """Create a minimal valid IFC file"""
-
-    ifc4_file = ifcplus.api.project.create_ifc4_file(
-        model_view_definition="ReferenceView_V1.2",
-        precision=1e-4,
-    )
-
-    project = ifc4_file.by_type(type="IfcProject", include_subtypes=False)[0]
-
-    site = ifcopenshell.api.root.create_entity(
-        file=ifc4_file,
-        ifc_class="IfcSite",
-        name="Site-01",
-    )
-    ifcopenshell.api.aggregate.assign_object(
-        file=ifc4_file,
-        products=[site],
-        relating_object=project,
-    )
-    ifcplus.api.placement.edit_object_placement(
-        product=site,
-        place_object_relative_to_parent=True,
-    )
-
-    return ifc4_file
-
-
-@pytest.fixture
-def ifc_file_with_one_element() -> ifcopenshell.file:
-    """Create a minimal valid IFC file with one element"""
-
-    ifc4_file = ifcplus.api.project.create_ifc4_file(
-        model_view_definition="ReferenceView_V1.2",
-        precision=1e-4,
-    )
-
-    project = ifc4_file.by_type(type="IfcProject", include_subtypes=False)[0]
-
-    site = ifcopenshell.api.root.create_entity(
-        file=ifc4_file,
-        ifc_class="IfcSite",
-        name="Site-01",
-    )
-    ifcopenshell.api.aggregate.assign_object(
-        file=ifc4_file,
-        products=[site],
-        relating_object=project,
-    )
-    ifcplus.api.placement.edit_object_placement(
-        product=site,
-        place_object_relative_to_parent=True,
-    )
-
-    element = ifcopenshell.api.root.create_entity(
-        file=ifc4_file,
-        ifc_class="IfcBuildingElementProxy",
-        name="Element-01",
-        predefined_type=None,
-    )
-    ifcopenshell.api.spatial.assign_container(
-        file=ifc4_file,
-        products=[element],
-        relating_structure=site,
-    )
-    ifcplus.api.placement.edit_object_placement(
-        product=element,
-        place_object_relative_to_parent=True,
-    )
-
-    return ifc4_file
