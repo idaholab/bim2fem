80b5618dcef227f53c78b7338fb47a925952209a
Nicholas Crowder <Nicholas.Crowder@inl.gov>
2025-11-20 21:24:27 -0500

    remove bim2glb

--- web/app.py
+++ web/app.py
@@ -13,9 +13,7 @@ sys.path.insert(0, str(Path(__file__).parent.parent / "src"))
 import json
 import ifcopenshell
 import ifcplus.api.project
-import bim2glb.convert_ifc_to_glb
 import bim2fem.convert_ifc_to_fem
-import bim2fem.recreate_fem_with_3d_body_shape_representation
 from bim2fem.adjust_element_connectivity_of_building_fem import (
     adjust_element_connectivity_of_ifc4_sav_file,
 )
@@ -37,8 +35,8 @@ app = Flask(
 )
 
 JOB_OPTIONS = [
-    "View IFC File",
-    "Convert IFC to GLB",
+    # "View IFC File",
+    # "Convert IFC to GLB",
     "Convert IFC to Finite Element Model",
     # "Adjust Element Connectivity of Finite Element Model",
 ]
@@ -84,7 +82,8 @@ def index():
         if selected_job == "View IFC File":
             return redirect(location=url_for(endpoint="view_ifc"))
         elif selected_job == "Convert IFC to GLB":
-            return redirect(location=url_for(endpoint="convert_to_glb"))
+            return "This feature is currently unavailable"
+            # return redirect(location=url_for(endpoint="convert_to_glb"))
         elif selected_job == "Convert IFC to Finite Element Model":
             # return redirect(location=url_for(endpoint="convert_to_fem"))
             return "This feature is currently unavailable"
@@ -193,185 +192,10 @@ def convert_to_fem_done(
     output_ifc_filename: str,
 ):
 
-    if request.method == "POST":
-
-        # Get the file path for the IFC4 SAV file
-        ifc4_sav_file_path = os.path.abspath(
-            os.path.join(
-                PATH_TO_OUTPUT_DIRECTORY,
-                output_ifc_filename,
-            )
-        )
-
-        # Open the IFC file as an ifcopenshell.file object
-        ifc4_sav_file = ifcopenshell.open(path=ifc4_sav_file_path)
-        if not isinstance(ifc4_sav_file, ifcopenshell.file):
-            return "IFC file could not be opened by IfcOpenShell"
-
-        # Recreate the IFC4 SAV file with Body Shape Representations
-        recreated_ifc4_sav_file = bim2fem.recreate_fem_with_3d_body_shape_representation.recreate_ifc4_sav_with_3d_body_shape_representation(
-            ifc4_sav_file=ifc4_sav_file,
-            view_option="Wireframe_3D",
-        )
-
-        # Get the file path for the IFC4 SAV file recreated with Body Shape
-        # Representations
-        recreated_ifc4_sav_file_path = os.path.abspath(
-            os.path.join(
-                PATH_TO_OUTPUT_DIRECTORY,
-                output_ifc_filename.replace(
-                    ".ifc",
-                    "_recreated_with_body_representations.ifc",
-                ),
-            )
-        )
-
-        # Write the Recreated IFC4 SAV file to disk
-        ifcplus.api.project.write_to_ifc_spf(
-            ifc4_file=recreated_ifc4_sav_file,
-            file_path=recreated_ifc4_sav_file_path,
-            add_annotations=False,
-        )
-
-        # Get file path for the GLB
-        output_glb_filename = output_ifc_filename.replace(".ifc", ".glb")
-        output_glb_file_path = os.path.abspath(
-            os.path.join(
-                PATH_TO_OUTPUT_DIRECTORY,
-                output_glb_filename,
-            )
-        )
-
-        # Convert the IFC to GLB
-        output_glb_file_path = bim2glb.convert_ifc_to_glb.convert_ifc_to_glb(
-            ifc_input_filename=recreated_ifc4_sav_file_path,
-            glb_output_filename=output_glb_file_path,
-            show_global_coordinate_system_axes=False,
-            delete_intermediate_glb_file=True,
-            store_metadata_in_glb_nodes=True,
-        )
-
-        # Create JSON Metadata file from the IFC
-        metadata_file_path = create_metadata_json_file_from_ifc(
-            ifc4_file=recreated_ifc4_sav_file
-        )
-        metadata_filename = os.path.split(metadata_file_path)[1]
-
-        print(f"output_glb_filename: {output_glb_filename}")
-        print(f"metadata_filename: {metadata_filename}")
-
-        return redirect(
-            location=url_for(
-                endpoint="viewer_for_glb_from_ifc",
-                filename=output_glb_filename,
-                metadata_filename=metadata_filename,
-            )
-        )
-
     return render_template(
         template_name_or_list="convert_to_fem_done.html",
         input_ifc_filename=input_ifc_filename,
         output_ifc_filename=output_ifc_filename,
-    )
-
-
-@app.route(rule="/convert_to_glb", methods=["GET", "POST"])
-def convert_to_glb():
-    if request.method == "POST":
-
-        # Get IFC File in FileStorage
-        if "ifc_file" not in request.files:
-            return "No file uploaded"
-        ifc_file_in_FileStorage = request.files["ifc_file"]
-        if not isinstance(ifc_file_in_FileStorage.filename, str):
-            return "Filename is not string"
-        if ifc_file_in_FileStorage.filename == "":
-            return "No selected file"
-
-        # Get the input file path for the IFC
-        input_ifc_filename = ifc_file_in_FileStorage.filename
-        input_ifc_file_path = os.path.abspath(
-            os.path.join(PATH_TO_INPUT_DIRECTORY, input_ifc_filename)
-        )
-
-        # Save the IFC to the input folder
-        ifc_file_in_FileStorage.save(dst=input_ifc_file_path)
-
-        # Open the IFC file as an ifcopenshell.file object
-        ifcopenshell_file = ifcopenshell.open(path=input_ifc_file_path)
-        if not isinstance(ifcopenshell_file, ifcopenshell.file):
-            return "IFC file could not be opened by IfcOpenShell"
-
-        # Get output file path for the GLB
-        output_glb_filename = input_ifc_filename.replace(".ifc", ".glb")
-        output_glb_file_path = os.path.abspath(
-            os.path.join(PATH_TO_OUTPUT_DIRECTORY, output_glb_filename)
-        )
-
-        # Convert the IFC to GLB
-        output_glb_file_path = bim2glb.convert_ifc_to_glb.convert_ifc_to_glb(
-            ifc_input_filename=input_ifc_file_path,
-            glb_output_filename=output_glb_file_path,
-            show_global_coordinate_system_axes=False,
-            delete_intermediate_glb_file=True,
-            store_metadata_in_glb_nodes=True,
-        )
-
-        # Redirect to Process Completion page
-        return redirect(
-            location=url_for(
-                endpoint="convert_to_glb_done",
-                input_ifc_filename=input_ifc_filename,
-                output_glb_filename=output_glb_filename,
-            )
-        )
-
-    return render_template(
-        template_name_or_list="convert_to_glb.html",
-    )
-
-
-@app.route(
-    "/convert_to_glb_done/<input_ifc_filename>/<output_glb_filename>",
-    methods=["GET", "POST"],
-)
-def convert_to_glb_done(
-    input_ifc_filename: str,
-    output_glb_filename: str,
-):
-    if request.method == "POST":
-
-        # Get the input file path for the IFC
-        input_ifc_file_path = os.path.abspath(
-            os.path.join(PATH_TO_INPUT_DIRECTORY, input_ifc_filename)
-        )
-
-        # Open the IFC file as an ifcopenshell.file object
-        ifcopenshell_file = ifcopenshell.open(path=input_ifc_file_path)
-        if not isinstance(ifcopenshell_file, ifcopenshell.file):
-            return "IFC file could not be opened by IfcOpenShell"
-
-        # Create JSON Metadata file from the IFC
-        metadata_file_path = create_metadata_json_file_from_ifc(
-            ifc4_file=ifcopenshell_file
-        )
-        metadata_filename = os.path.split(metadata_file_path)[1]
-
-        print(f"output_glb_filename: {output_glb_filename}")
-        print(f"metadata_filename: {metadata_filename}")
-
-        return redirect(
-            location=url_for(
-                endpoint="viewer_for_glb_from_ifc",
-                filename=output_glb_filename,
-                metadata_filename=metadata_filename,
-            )
-        )
-
-    return render_template(
-        template_name_or_list="convert_to_glb_done.html",
-        input_ifc_filename=input_ifc_filename,
-        output_glb_filename=output_glb_filename,
     )
 
 
@@ -384,111 +208,6 @@ def download_file(
         directory=directory,
         path=filename,
         as_attachment=True,
-    )
-
-
-def create_metadata_json_file_from_ifc(
-    ifc4_file: ifcopenshell.file,
-) -> str:
-
-    metadata_file_path = os.path.abspath(
-        os.path.join(PATH_TO_METADATA_DIRECTORY, "metadata.json")
-    )
-    # metadata_file_path = os.path.join("metadata", "metadata.json")
-
-    ifc_products = ifc4_file.by_type(type="IfcProduct", include_subtypes=True)
-
-    objects = []
-    for ifc_product in ifc_products:
-        # if ifc_product.is_a() == "IfcOpeningElement":
-        #     continue
-        objects.append(
-            {
-                "class": ifc_product.is_a(),
-                "globalid": ifc_product.GlobalId,
-                "name": ifc_product.Name,
-                "description": ifc_product.Description,
-                "objecttype": ifc_product.ObjectType,
-                "requirement": "None",
-            },
-        )
-
-    data = {"objects": objects}
-
-    with open(file=metadata_file_path, mode="w") as json_file:
-        json.dump(data, json_file, indent=4)
-
-    return metadata_file_path
-
-
-@app.route("/view_ifc", methods=["GET", "POST"])
-def view_ifc():
-    if request.method == "POST":
-
-        # Get IFC File in FileStorage
-        if "ifc_file" not in request.files:
-            return "No file uploaded"
-        ifc_file_in_FileStorage = request.files["ifc_file"]
-        if not isinstance(ifc_file_in_FileStorage.filename, str):
-            return "Filename is not string"
-        if ifc_file_in_FileStorage.filename == "":
-            return "No selected file"
-
-        # Get the input file path for the IFC
-        input_ifc_filename = ifc_file_in_FileStorage.filename
-        input_ifc_file_path = os.path.abspath(
-            os.path.join(PATH_TO_INPUT_DIRECTORY, input_ifc_filename)
-        )
-
-        # Save the IFC to the input folder
-        ifc_file_in_FileStorage.save(dst=input_ifc_file_path)
-
-        # Open the IFC file as an ifcopenshell.file object
-        ifcopenshell_file = ifcopenshell.open(path=input_ifc_file_path)
-        if not isinstance(ifcopenshell_file, ifcopenshell.file):
-            return "IFC file could not be opened by IfcOpenShell"
-
-        # Get output file path for the GLB
-        output_glb_filename = input_ifc_filename.replace(".ifc", ".glb")
-        output_glb_file_path = os.path.abspath(
-            os.path.join(PATH_TO_OUTPUT_DIRECTORY, output_glb_filename)
-        )
-
-        # Convert the IFC to GLB
-        output_glb_file_path = bim2glb.convert_ifc_to_glb.convert_ifc_to_glb(
-            ifc_input_filename=input_ifc_file_path,
-            glb_output_filename=output_glb_file_path,
-            show_global_coordinate_system_axes=False,
-            delete_intermediate_glb_file=True,
-            store_metadata_in_glb_nodes=True,
-        )
-
-        # Create JSON Metadata file from the IFC
-        metadata_file_path = create_metadata_json_file_from_ifc(
-            ifc4_file=ifcopenshell_file
-        )
-        metadata_filename = os.path.split(metadata_file_path)[1]
-
-        print(f"output_glb_filename: {output_glb_filename}")
-        print(f"metadata_filename: {metadata_filename}")
-
-        return redirect(
-            location=url_for(
-                endpoint="viewer_for_glb_from_ifc",
-                filename=output_glb_filename,
-                metadata_filename=metadata_filename,
-            )
-        )
-
-    return render_template("view_ifc.html")
-
-
-@app.route("/viewer_for_glb_from_ifc/<filename>/<metadata_filename>")
-def viewer_for_glb_from_ifc(filename, metadata_filename):
-    return render_template(
-        "viewer_for_glb_from_ifc.html",
-        glb_filename=filename,
-        metadata_filename=metadata_filename,
     )
 
 
