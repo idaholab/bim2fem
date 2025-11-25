9057e1e860c21f71750a6438ac92de061ce01f4d
Nicholas Crowder <Nicholas.Crowder@inl.gov>
2025-11-25 14:59:41 -0500

    add Revit model DTV example to pytest

--- tests/core/convert/test_convert.py
+++ tests/core/convert/test_convert.py
@@ -42,7 +42,7 @@ class TestConvertFrameStructureToFEM:
         pprint(logger.statements)
         assert len(logger.statements) == 0
 
-    def test_convert_revit_steel_construction_to_fem(
+    def test_convert_revit_steel_construction_rv_to_fem(
         self,
     ):
 
@@ -59,6 +59,35 @@ class TestConvertFrameStructureToFEM:
 
         output_path = str(
             OUTPUT_DIR_FOR_CONVERT / "SteelConstruction_RV_converted_to_SAV.ifc"
+        )
+        bim2fem.ifcplus.api.project.write_to_ifc_spf(
+            ifc4_file=ifc4sav_file,
+            file_path=output_path,
+            add_annotations=True,
+        )
+
+        logger = ifcopenshell.validate.json_logger()
+        ifcopenshell.validate.validate(output_path, logger, express_rules=True)
+        pprint(logger.statements)
+        assert len(logger.statements) == 0
+
+    def test_convert_revit_steel_construction_dtv_to_fem(
+        self,
+    ):
+
+        ifc4_rv_file = ifcopenshell.open(
+            path=str(INPUT_DIR / "SteelConstruction_DTV.ifc"),
+        )
+        ifc4_rv_file = cast(ifcopenshell.file, ifc4_rv_file)
+
+        ifc4sav_file = convert_frame_structure_to_fem(
+            ifc4_source_file=ifc4_rv_file,
+            element_deselection_query='type *= "Footing"',
+            region="Europe",
+        )
+
+        output_path = str(
+            OUTPUT_DIR_FOR_CONVERT / "SteelConstruction_DTV_converted_to_SAV.ifc"
         )
         bim2fem.ifcplus.api.project.write_to_ifc_spf(
             ifc4_file=ifc4sav_file,
